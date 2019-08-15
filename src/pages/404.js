aaef5aa71fbd4ff169729579f0fda4e40b5fb74d
Zach Rodis <zach@Zachs-MacBook-Air.local>
2019-08-15 01:26:50 -0400

    troubleshoot deploy: removed svg

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,14 +2,15 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Kimbirdly from '../components/Kimbirdly'
+// import Kimbirdly from '../components/Kimbirdly'
+// <Kimbirdly />
+
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <div className="flexCenterColumn" style={{paddingTop:'20px'}}>
       <h1>Oops you're lost</h1>
-      <Kimbirdly />
     </div>
   </Layout>
 )
--- src/pages/birdpage.js
+++ src/pages/birdpage.js
@@ -3,13 +3,13 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import {Spring } from 'react-spring/renderprops'
 
-import Kimbirdly from '../components/Kimbirdly'
+// import Kimbirdly from '../components/Kimbirdly'
+// <Kimbirdly />
 
 
 const BirdPage = ({data}) => (
   <Layout>
     <SEO title="Bird" />
-    <Kimbirdly />
   </Layout>
 )
 
