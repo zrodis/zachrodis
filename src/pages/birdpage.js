84c4f83d3dcfbc5fba67598cf2869e5aab21dc37
Zach Rodis <zach@Zachs-MacBook-Air.local>
2019-08-15 07:08:40 -0400

    testing svg deploy

--- src/pages/birdpage.js
+++ src/pages/birdpage.js
@@ -1,15 +1,15 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import {Spring } from 'react-spring/renderprops'
 
-// import Kimbirdly from '../components/Kimbirdly'
-// <Kimbirdly />
+import Kimbirdly from '../components/Kimbirdly'
 
 
 const BirdPage = ({data}) => (
   <Layout>
     <SEO title="Bird" />
+    <Kimbirdly />
+
   </Layout>
 )
 
